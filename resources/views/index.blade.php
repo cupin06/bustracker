a537d269969add96d0b6027e8a4c502d6ca1d2a1
Muhammad Faiz <muhdfaizhalim@gmail.com>
2015-11-22 12:04:36 +0800

    Fix css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,7 +52,7 @@
         duration: 300, // Speed in ms how fast you want it to be
         transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
         clickSelector: '.toggle-panel', // Enables toggling when clicking elements of this class
-        distanceX: '30%', // Size fo the toggle
+        distanceX: '50%', // Size fo the toggle
         enableEscapeKey: true // Clicking Esc will close the panel
     });
 
